75a33d7fb4e1c1cd9ff8b7349bd125093a86fae4
malvesbertoni <malvesbertoni@gmail.com>
2021-08-10 16:26:31 -0300

    Finished SecondSection

--- src/components/SecondSection/SecondSection.js
+++ src/components/SecondSection/SecondSection.js
@@ -1,4 +1,5 @@
 import "./SecondSection.css";
+import { Link } from "react-router-dom";
 import imgEgg from "../../images/desktop/image-transform.jpg";
 import imgCup from "../../images/desktop/image-stand-out.jpg";
 
@@ -9,13 +10,19 @@ const SecondSection = () => {
         {/* Classname of the divs will be "ss-something" 
             The "ss" is short for "second-section"  */}
         <div className="ss-information">
-          <h1>Transform your brand</h1>
+          <h1>
+            Transform your
+            <br /> brand
+          </h1>
           <p>
-            We are a full-service creative agency specializing in helping brands
-            grow fast. Engage your clients through compelling visuals that do
-            most of the marketing for you.
+            We are a full-service creative agency specializing in
+            <br /> helping brands grow fast. Engage your clients through
+            <br /> compelling visuals that do most of the marketing for you.
           </p>
-          <h2>LEARN MORE</h2>
+
+          <Link to="/about" className="ss-text-transform">
+            LEARN MORE
+          </Link>
         </div>
         <div className="ss-full-image">
           <img src={imgEgg} alt="Egg" />
@@ -23,29 +30,40 @@ const SecondSection = () => {
         <div className="ss-full-image">
           <img src={imgCup} alt="Cup" />
         </div>
-        <div className="ss-information">
-          <h1>Stand out to the right audience</h1>
+        <div className="ss-information stand-out">
+          <h1>
+            Stand out to the right
+            <br /> audience
+          </h1>
           <p>
             Using a collaborative formula of designers, researchers,
-            photographers, videographers, and copywriters, we'll build and
-            extend your brand in digital places.
+            <br />
+            photographers, videographers, and copywriters, we'll
+            <br /> build and extend your brand in digital places.
           </p>
-          <h2>LEARN MORE</h2>
+          <Link to="/about" className="ss-text-stand-out">
+            LEARN MORE
+          </Link>
         </div>
-        <div className="ss-image-info">
-          <h1>Graphic Design</h1>
-          <p>
-            Great design makes you memorable. We deliver artwork that
-            underscores your brand message and captuers potential clients'
-            attention.
-          </p>
+        <div className="ss-image-info graphic-design">
+          <div className="ss-image-info-subtitle">
+            <h1>Graphic Design</h1>
+            <p>
+              Great design makes you memorable. We deliver
+              <br /> artwork that underscores your brand message
+              <br /> and captuers potential clients' attention.
+            </p>
+          </div>
         </div>
-        <div className="ss-image-info">
-          <h1>Photography</h1>
-          <p>
-            Increase your credibility by getting the most stunning, high-quality
-            photos that improve your business image.
-          </p>
+        <div className="ss-image-info photography">
+          <div className="ss-image-info-subtitle">
+            <h1>Photography</h1>
+            <p>
+              Increase your credibility by getting the most
+              <br /> stunning, high-quality photos that improve your
+              <br /> business image.
+            </p>
+          </div>
         </div>
       </div>
     </section>
